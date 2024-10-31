0a5041380368e648e3057d2398816a4a690a3904
jhonatanjacome07 <jhonatanjacome07@gmail.com>
2024-10-31 16:39:52 -0500

    Configuramos el archivo Dockerfile para hacer un deploy en railway

---
+++ src/main/java/com/apirest/apirest/Controllers/ProductoController.java
@@ -0,0 +1,56 @@
+package com.apirest.apirest.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.apirest.Entities.Producto;
+import com.apirest.apirest.Repositories.ProductoRepository;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping
+    public List<Producto> getAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Producto getProductoById(@PathVariable Long id) {
+        return productoRepository.findById(id).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+    }
+
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @PutMapping("/{id}")
+    public Producto updateProducto(@PathVariable Long id, @RequestBody Producto detallesproducto) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        producto.setNombre(detallesproducto.getNombre());
+        producto.setPrecio(detallesproducto.getPrecio());
+        return productoRepository.save(producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProducto(@PathVariable Long id) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado: " + id));
+        productoRepository.delete(producto);
+        return "El producto con id " + id + " ha sido eliminado";
+    }
+}
